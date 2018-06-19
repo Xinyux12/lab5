0af783f8c1551b26bac1a03bafbbb5e191e9d832
xinran <xinranyu1108@outlook.com>
2018-06-19 12:31:29 +1200

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { map } from 'rxjs/operator';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import {Observable } from 'rxjs';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent  {
+  title = "Sophie's App";
+  name= 'Ayesha';
+        
+ constructor(private http: HttpClient) {}
+ 
+ ngOnInit(){
+     
+    
+    
+ }
+ 
+   
 }
